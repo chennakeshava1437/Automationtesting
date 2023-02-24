2f2d01247a30d1848bd6e52ba86373439be758ca
ychen <ychen@ideapad>
2023-02-24 15:57:40 +0530

    This is My Automation repository

---
+++ Sampleproject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ychen
+ *
+ */
+module Sampleproject {
+}
---
+++ Sampleproject/src/samplecode/ArmStrong.java
@@ -0,0 +1,21 @@
+package samplecode;
+
+public class ArmStrong {
+	public static void main(String[] args) 
+	{
+		int number=153, originalnumber, remainder, result=0;
+		originalnumber = number;
+		while(originalnumber !=0)
+		{
+			remainder=originalnumber%10;
+			result +=Math.pow(remainder, 3);
+			originalnumber /=10;
+		}
+		if(result ==number)
+			System.out.println(number+"is an armstrong");
+		else
+			System.out.println(number+"is not an armstrong");
+	}
+}	
+
+
